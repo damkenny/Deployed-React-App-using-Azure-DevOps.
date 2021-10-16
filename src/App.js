3a86562849513dd96d8b5a5eb0a6b605d6bd071a
ADENIYE KEHINDE <adeniyekehinde0@gmail.com>
2021-10-16 20:21:50 +0000

    second push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sample react App 
         </a>
       </header>
     </div>
